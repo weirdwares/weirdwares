0d952fd84605adc65b0501c1833a0944a77ac05a
Dave Hale <dave@moodtracker.com>
2020-10-13 22:28:08 -0600

    Adds environment variable

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>
+					(your are in the "{process.env.REACT_APP_THE_ENVIRONMENT}" environment)
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
